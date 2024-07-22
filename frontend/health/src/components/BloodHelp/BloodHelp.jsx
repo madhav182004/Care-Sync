dce8ed970087a9f845ec7239fdc424f49163cce5
Madhav Mundhra <madhavmundhra221@gmail.com>
2024-07-22 21:28:46 +0530

    Updated logic for Blood Help

--- frontend/health/src/components/BloodHelp/BloodHelp.jsx
+++ frontend/health/src/components/BloodHelp/BloodHelp.jsx
@@ -29,42 +29,40 @@ const BloodHelp = () => {
       />
       <hr />
       <table className="table-auto w-full">
-  <thead>
-    <tr className="bg-gray-200 text-left">
-      <th className="px-4 py-2">Id</th>
-      <th className="px-4 py-2">State</th>
-      <th className="px-4 py-2"></th>
-      <th className="px-4 py-2">Hospital</th>
-      <th className="px-4 py-2">Contact No</th>
-      <th className="px-4 py-2">Address</th>
-    </tr>
-  </thead>
-  <tbody>
-  {dataToDisplay.length > 0 ? (
-    dataToDisplay.map((data, index) => (
-      <tr
-        key={data.sr_no}
-        className={`${
-          index % 2 === 0 ? "bg-white" : "bg-gray-100"
-        } hover:bg-gray-300`}
-      >
-        <td className="border px-4 py-2">{data.sr_no}</td>
-        <td className="border px-4 py-2">{data._state}</td>
-        <td className="border px-4 py-2">{data.pincode}</td>
-        <td className="border px-4 py-2">{data._blood_bank_name}</td>
-        <td className="border px-4 py-2">{data._contact_no}</td>
-        <td className="border px-4 py-2">{data._address}</td>
-      </tr>
-    ))
-  ) : (
-    <tr>
-      <td colSpan="6" className="text-center py-4">No Record Found</td>
-    </tr>
-  )}
-</tbody>
-
-</table>
-
+        <thead>
+          <tr className="bg-gray-200 text-left">
+            <th className="px-4 py-2">Id</th>
+            <th className="px-4 py-2">State</th>
+            <th className="px-4 py-2">Pincode</th>
+            <th className="px-4 py-2">Hospital</th>
+            <th className="px-4 py-2">Contact No</th>
+            <th className="px-4 py-2">Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataToDisplay.length > 0 ? (
+            dataToDisplay.map((data, index) => (
+              <tr
+                key={data.sr_no}
+                className={`${
+                  index % 2 === 0 ? "bg-white" : "bg-gray-100"
+                } hover:bg-gray-300`}
+              >
+                <td className="border px-4 py-2">{data.sr_no}</td>
+                <td className="border px-4 py-2">{data._state}</td>
+                <td className="border px-4 py-2">{data.pincode}</td>
+                <td className="border px-4 py-2">{data._blood_bank_name}</td>
+                <td className="border px-4 py-2">{data._contact_no}</td>
+                <td className="border px-4 py-2">{data._address}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="6" className="text-center py-4">No Record Found</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
